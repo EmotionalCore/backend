a4ea41a1d0137f81bad8b0d550b0c31d16a00ebb
L33gn21 <l33gn21@gmail.com>
2025-01-09 21:57:58 +0900

    Swagger 적용 / WorkController 설계

---
+++ src/main/java/com/example/project/emotionCore/dto/NovelAndPoemPreviewDTO.java
@@ -0,0 +1,18 @@
+package com.example.project.emotionCore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Builder
+@Data
+@AllArgsConstructor
+public class NovelAndPoemPreviewDTO{
+    private int id;
+    private List<AuthorInfoDTO> authorInfos;
+    private String title;
+    private String description;
+    private String coverImageUrl;
+}
