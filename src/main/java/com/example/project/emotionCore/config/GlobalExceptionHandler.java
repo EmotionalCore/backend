a1001cbecbb25d84d9719ac2c7f635ba78007d46
seapeon <seapeon@naver.com>
2025-01-22 22:19:02 +0900

    LoginBlock

--- src/main/java/com/example/project/emotionCore/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/project/emotionCore/config/GlobalExceptionHandler.java
@@ -4,6 +4,7 @@ import com.example.project.emotionCore.dto.ErrorResponse;
 import com.example.project.emotionCore.exception.CustomBadRequestException;
 import jakarta.persistence.EntityNotFoundException;
 import org.apache.coyote.BadRequestException;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,5 +29,18 @@ public class GlobalExceptionHandler {
         //ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "서버에서 문제가 발생했습니다."); // 실제 배포시에는 이걸로
         ErrorResponse errorResponse = new ErrorResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), ex.getMessage());
         return ResponseEntity.internalServerError().body(errorResponse);
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolation(DataIntegrityViolationException ex) {
+        // 사용자에게 유니크 제약 조건 위반 메시지 전달
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage());
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 }
--- src/main/java/com/example/project/emotionCore/dto/MemberDTO.java
+++ src/main/java/com/example/project/emotionCore/dto/MemberDTO.java
@@ -1,6 +1,9 @@
 package com.example.project.emotionCore.dto;
 
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -11,8 +14,20 @@ import lombok.Setter;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MemberDTO {
+
     private Long id;
+
+    @Size(min=2, max=10)
     private String username;
+
+    @Size(max=50)
+    @Email
     private String email;
+
+    @Size(min = 8, max = 15)
+    @Pattern(
+            regexp = "^(?=.*[0-9!@#$%^&*]).+$",
+            message = "비밀번호는 반드시 숫자 또는 특수문자를 포함해야 합니다."
+    )
     private String password;
 }
