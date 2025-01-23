3b64215c533176d4582a8d465d7485df331baa76
ysr9517 <ysr9517@naver.com>
2025-01-24 00:40:35 +0900

    전체, 신규, 이달의 인기 작품 수정 완료

---
+++ src/main/java/com/example/project/emotionCore/Repository/AuthorRepository.java
@@ -0,0 +1,11 @@
+package com.example.project.emotionCore.Repository;
+
+import com.example.project.emotionCore.domain.Author;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+    List<Author> findAllByOrderByIdDesc(Pageable pageable);
+}
