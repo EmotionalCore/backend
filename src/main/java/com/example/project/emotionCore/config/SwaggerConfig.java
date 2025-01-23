588bb15a324f0b30fe00ac428fce75db9d1faf0e
ysr9517 <ysr9517@naver.com>
2025-01-23 17:58:20 +0900

    Swagger 다듬기(토큰유무에 따라 테스트 가능)

--- src/main/java/com/example/project/emotionCore/config/SwaggerConfig.java
+++ src/main/java/com/example/project/emotionCore/config/SwaggerConfig.java
@@ -1,6 +1,9 @@
 package com.example.project.emotionCore.config;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,11 +12,23 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI openAPI() {
+        final String securitySchemeName = "bearerAuth";
         Info info = new Info()
                 .version("v1.0") //버전
                 .title("EmotionCore") //이름
                 .description("EmotionCores"); //설명
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(
+                        new Components()
+                                .addSecuritySchemes(securitySchemeName,
+                                        new SecurityScheme()
+                                                .name(securitySchemeName)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                )
                 .info(info);
     }
 
