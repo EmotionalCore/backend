2be3cf2b570deef6e6699c87aed8607971fc26e9
ysr9517 <ysr9517@naver.com>
2025-02-17 11:22:02 +0900

    데이터 네이밍 수정

--- src/main/java/com/example/project/emotionCore/Service/AuthorService.java
+++ src/main/java/com/example/project/emotionCore/Service/AuthorService.java
@@ -24,7 +24,7 @@ public class AuthorService {
         List<AuthorDTO> data = new ArrayList<>();
         for (Author author : entity) {
             AuthorDTO dto = modelMapper.map(author, AuthorDTO.class);
-            dto.setName(memberRepository.findById((long) dto.getId()).get().getUsername());
+            dto.setAuthorName(memberRepository.findById((long) dto.getId()).get().getUsername());
             dto.setProfileImageUrl(memberRepository.findById((long) dto.getId()).get().getProfileImageUrl());
             data.add(dto);
         }
