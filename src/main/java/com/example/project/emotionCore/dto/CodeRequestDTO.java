f0cc59f4e5b0f2ce9fd0d50f292a2c085dba6206
ysr9517 <ysr9517@naver.com>
2025-04-15 12:43:56 +0900

    카카오 로그인 후 Jwt토큰 형식으로 반환 및 Get -> Post 수정

---
+++ src/main/java/com/example/project/emotionCore/dto/CodeRequestDTO.java
@@ -0,0 +1,14 @@
+package com.example.project.emotionCore.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CodeRequestDTO {
+    private String code;
+}
