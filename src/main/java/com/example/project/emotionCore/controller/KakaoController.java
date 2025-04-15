3b8659f75c3f2774693d46266a3bb39a17350356
ysr9517 <ysr9517@naver.com>
2025-04-15 12:43:33 +0900

    카카오 로그인 후 Jwt토큰 형식으로 반환 및 Get -> Post 수정

--- src/main/java/com/example/project/emotionCore/controller/KakaoController.java
+++ src/main/java/com/example/project/emotionCore/controller/KakaoController.java
@@ -1,15 +1,11 @@
 package com.example.project.emotionCore.controller;
 
 import com.example.project.emotionCore.Service.KakaoService;
+import com.example.project.emotionCore.dto.CodeRequestDTO;
 import com.example.project.emotionCore.dto.JwtTokenDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -24,7 +20,7 @@ public class KakaoController {
     }
 
     @PostMapping()
-    public JwtTokenDTO kakaoLogin(@RequestParam String code) {
-        return kakaoService.socialLogin(code);
+    public JwtTokenDTO kakaoLogin(@RequestBody CodeRequestDTO dto) {
+        return kakaoService.socialLogin(dto.getCode());
     }
 }
