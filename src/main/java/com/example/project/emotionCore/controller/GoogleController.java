e3a79458b65fda90157245dc1af7d20e5b3e6b2c
L33gn21 <l33gn21@gmail.com>
2025-05-04 17:29:41 +0900

    Access는 body, refresh는 header

--- src/main/java/com/example/project/emotionCore/controller/GoogleController.java
+++ src/main/java/com/example/project/emotionCore/controller/GoogleController.java
@@ -23,19 +23,9 @@ public class GoogleController {
     }
 
     @PostMapping("/code/{registrationId}")
-    public ResponseEntity<Void> googleLogin(@RequestBody CodeRequestDTO dto) {
+    public ResponseEntity<String> googleLogin(@RequestBody CodeRequestDTO dto) {
         JwtTokenDTO tokens = googleService.socialLogin(dto.getCode());
 
-        // Access Token 쿠키 생성
-        ResponseCookie accessTokenCookie = ResponseCookie.from("accessToken", tokens.getAccessToken())
-                .httpOnly(true)    // 클라이언트 JavaScript에서 접근 불가
-                .secure(true)      // HTTPS 통신에서만 전송
-                .path("/")         // 쿠키 경로
-                .maxAge(3600)      // 1시간 유지
-                .sameSite("Lax")
-                .build();
-
-        // Refresh Token 쿠키 생성
         ResponseCookie refreshTokenCookie = ResponseCookie.from("refreshToken", tokens.getRefreshToken())
                 .httpOnly(true)
                 .secure(true)
@@ -46,8 +36,7 @@ public class GoogleController {
 
         return ResponseEntity
                 .ok()
-                .header(HttpHeaders.SET_COOKIE, accessTokenCookie.toString())
                 .header(HttpHeaders.SET_COOKIE, refreshTokenCookie.toString())
-                .build();
+                .body(tokens.getAccessToken());
     }
 }
